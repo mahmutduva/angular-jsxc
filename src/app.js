256a3c073677f2773b7915deab6ad4ace7a86a16
mahmutduva <mahmut.duva@freebirdairlines.com>
2016-06-03 11:00:38 +0300

    jsxc run on angular directive

--- src/app.js
+++ src/app.js
@@ -5,4 +5,19 @@ angular
 
 function appController($scope) {
     var vm = this;
+
+    vm.config = {
+      url: 'https://jabber.freebirdairlines.com:5288/http-bind',
+      domain: 'freebird.local',
+      resource: 'cockpitweb',
+      overwrite: true,
+      onlogin: true
+    };
+
+    vm.data =  {
+      "jid": "mahmut.duva@freebird.local",
+      "sid": "9e8385ca119206c6f5c61638168e6226716accc4",
+      "rid": 3963496211
+    };
+
 }

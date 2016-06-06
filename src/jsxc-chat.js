3dc4aa714d3e4dada19c7bafe090fb9d6d7112f7
Mahmut Duva <mahmutduva@gmail.com>
2016-06-06 15:29:03 +0300

    add events to directive

--- src/jsxc-chat.js
+++ src/jsxc-chat.js
@@ -1,52 +1,82 @@
-
-
-
 angular
-  .module('jsxcChat', [])
-  .directive('jsxcChat', function(){
-    return{
-      restrict: 'EA',
-      link: link,
-      template: template,
-      scope: {
-        config : '=',
-        data: '='
-      }
-    }
-  })
+    .module('jsxcChat', [])
+    .directive('jsxcChat', function ($document) {
+        return {
+            restrict: 'EA',
+            link: function (scope, element, attrs) {
+                var settings = {
+                    xmpp: scope.config
+                };
 
-  function template(){
-    return '<div>Test</div>'
-  }
+                jsxc.init({
+                    logoutElement: $('#logout'),
+                    checkFlash: false,
+                    rosterAppend: 'body',
+                    root: ('../jsxc-build'),
+                    displayRosterMinimized: function () {
+                        return true;
+                    },
+                    otr: {
+                        enable: false
+                    },
 
-  function link(scope, element, attrs){
+                    loadSettings: function (username, password, cb) {
+                        cb(settings);
+                    },
+                    xmpp: {
+                        url: settings.xmpp.url,
+                        jid: scope.data.jid,
+                        sid: scope.data.sid,
+                        rid: scope.data.rid
+                    }
+                });
 
-      var settings = {
-        xmpp: scope.config
-      };
 
-      jsxc.init({
-         logoutElement: $('#logout'),
-         checkFlash: false,
-         rosterAppend: 'body',
-         root: ('../jsxc-build'),
-         displayRosterMinimized: function() {
-           console.log(scope.data)
-            return true;
-         },
-         otr:{
-           enable: false
-         },
-         loadSettings: function(username, password, cb) {
-            cb(settings);
-         },
-         xmpp: {
-            url: settings.xmpp.url,
-            jid: scope.data.jid,
-            sid: scope.data.sid,
-            rid: scope.data.rid
+                $document.on('restoreCompleted.jsxc', function () {
+                    if(scope.disableSettings){
+                        disableSettings();
+                    }
+
+                    if(scope.hideOtrButton){
+                        hideOtrButton();
+                    }
+                });
+
+
+                $document.on('show.window.jsxc',function () {
+                    if(scope.hideOtrButton){
+                        hideOtrButton();
+                    }
+                })
+
+
+
+
+
+            },
+            template: template,
+            scope: {
+                config: '=',
+                data: '=',
+                disableSettings: '@',
+                hideOtrButton:'@'
+            }
         }
-      });
+    })
 
 
-  }
+function template() {
+    return '<div>Test</div>'
+}
+
+
+function disableSettings() {
+    var element = angular.element(document.querySelector("li.jsxc_settings"));
+    element.remove();
+}
+
+
+function hideOtrButton() {
+    var element = angular.element(document.querySelector(".jsxc_otr"));
+    element.remove();
+}
